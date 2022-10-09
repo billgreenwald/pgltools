ff71b9ee74cec93be405f34840fe26db0c482cac
Bill Young Greenwald <billyounggreenwald@gmail.com>
2022-10-09 16:14:12 -0700

    added test case from issue #12 to ensure it passes in any future changes

--- tests/test_intersect1D.py
+++ tests/test_intersect1D.py
@@ -12,31 +12,66 @@ ASSET_DIR=Path(__file__).parent
 
 @Parametrization.autodetect_parameters()
 @Parametrization.case(name="baseline", allA=False, bA=False, wa=False, wb=False, v=False, u=False,
-                      d=0, anchA="Intersected_Anchor", expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.pgl"))
+                      d=0, anchA="Intersected_Anchor",
+                      file_a=ASSET_DIR/"fixtures/file.A.sorted.annotated.pgl",
+                      file_b=ASSET_DIR/"fixtures/file.B.annotated.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.pgl"))
 @Parametrization.case(name="all_annotations", allA=True, bA=False, wa=False, wb=False, v=False, u=False,
-                      d=0, anchA="Intersected_Anchor", expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.allA.pgl"))
+                      d=0, anchA="Intersected_Anchor",
+                      file_a=ASSET_DIR / "fixtures/file.A.sorted.annotated.pgl",
+                      file_b=ASSET_DIR / "fixtures/file.B.annotated.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.allA.pgl"))
 @Parametrization.case(name="b_annotations", allA=False, bA=True, wa=False, wb=False, v=False, u=False,
-                      d=0, anchA="Intersected_Anchor", expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.bA.pgl"))
+                      d=0, anchA="Intersected_Anchor",
+                      file_a=ASSET_DIR / "fixtures/file.A.sorted.annotated.pgl",
+                      file_b=ASSET_DIR / "fixtures/file.B.annotated.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.bA.pgl"))
 @Parametrization.case(name="dashWa", allA=False, bA=False, wa=True, wb=False, v=False, u=False,
-                      d=0, anchA="Intersected_Anchor", expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.wa.pgl"))
+                      d=0, anchA="Intersected_Anchor",
+                      file_a=ASSET_DIR / "fixtures/file.A.sorted.annotated.pgl",
+                      file_b=ASSET_DIR / "fixtures/file.B.annotated.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.wa.pgl"))
 @Parametrization.case(name="dashWb", allA=False, bA=False, wa=False, wb=True, v=False, u=False,
-                      d=0, anchA="Intersected_Anchor", expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.wb.pgl"))
+                      d=0, anchA="Intersected_Anchor",
+                      file_a=ASSET_DIR / "fixtures/file.A.sorted.annotated.pgl",
+                      file_b=ASSET_DIR / "fixtures/file.B.annotated.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.wb.pgl"))
 @Parametrization.case(name="dashV", allA=False, bA=False, wa=False, wb=False, v=True, u=False,
-                      d=0, anchA="Intersected_Anchor", expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.v.pgl"))
+                      d=0, anchA="Intersected_Anchor",
+                      file_a=ASSET_DIR / "fixtures/file.A.sorted.annotated.pgl",
+                      file_b=ASSET_DIR / "fixtures/file.B.annotated.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.v.pgl"))
 @Parametrization.case(name="dashU", allA=False, bA=False, wa=False, wb=False, v=False, u=True,
-                      d=0, anchA="Intersected_Anchor", expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.u.pgl"))
+                      d=0, anchA="Intersected_Anchor",
+                      file_a=ASSET_DIR / "fixtures/file.A.sorted.annotated.pgl",
+                      file_b=ASSET_DIR / "fixtures/file.B.annotated.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.u.pgl"))
 @Parametrization.case(name="Distance_5,000", allA=False, bA=False, wa=False, wb=False, v=False, u=False,
-                      d=5000, anchA="Intersected_Anchor", expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.d5000.pgl"))
+                      d=5000, anchA="Intersected_Anchor",
+                      file_a=ASSET_DIR / "fixtures/file.A.sorted.annotated.pgl",
+                      file_b=ASSET_DIR / "fixtures/file.B.annotated.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.d5000.pgl"))
 @Parametrization.case(name="Distance_500,000", allA=False, bA=False, wa=False, wb=False, v=False, u=False,
-                      d=500000, anchA="Intersected_Anchor", expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.d500000.pgl"))
+                      d=500000, anchA="Intersected_Anchor",
+                      file_a=ASSET_DIR / "fixtures/file.A.sorted.annotated.pgl",
+                      file_b=ASSET_DIR / "fixtures/file.B.annotated.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.d500000.pgl"))
 @Parametrization.case(name="anchorIntersectColumnName", allA=False, bA=False, wa=False, wb=False, v=False, u=False,
-                      d=0, anchA="some_arbitrary_name", expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.anchA.pgl"))
-def test_intersect_1d(allA,bA,wa,wb,v,u,d,anchA,expected_results_file):
+                      d=0, anchA="some_arbitrary_name",
+                      file_a=ASSET_DIR / "fixtures/file.A.sorted.annotated.pgl",
+                      file_b=ASSET_DIR / "fixtures/file.B.annotated.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.anchA.pgl"))
+@Parametrization.case(name="test_intrachromosomal_intersection", allA=False, bA=False, wa=True, wb=False, v=False, u=False,
+                      d=0, anchA="anchorIntersectColumnName",
+                      file_a=ASSET_DIR / "fixtures/intra_chromosome.A.pgl",
+                      file_b=ASSET_DIR / "fixtures/issue12_example.bed",
+                      expected_results_file = str(ASSET_DIR/"fixtures/intersect1D/intersect1D_expected_results.intrachrom.pgl"))
+def test_intersect_1d(allA,bA,wa,wb,v,u,d,anchA,file_a,file_b,expected_results_file):
 
     #get files
-    headerA,A=processFile(str(ASSET_DIR/"fixtures/file.A.sorted.annotated.pgl"))
+    headerA,A=processFile(file_a)
 
-    headerB,B=processBedFile(str(ASSET_DIR/"fixtures/file.B.annotated.bed"))
+    headerB,B=processBedFile(file_b)
 
     args={
         "allA":allA,
