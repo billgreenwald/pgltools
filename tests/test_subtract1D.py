834a06c5b7f8629bf33eb0736a9445468e5e4c1f
Bill Young Greenwald <billyounggreenwald@gmail.com>
2022-10-09 09:55:04 -0700

    added test for subtract1D

---
+++ tests/test_subtract1D.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('../Module/PyGLtools/')
+from subtract1D import subtract1D
+from pgltools_library import  processFile, processBedFile, compare_test_outputs
+import pytest
+from pathlib import Path
+from parametrization import Parametrization
+
+ASSET_DIR=Path(__file__).parent
+
+##can parameterize this in a sec
+
+@Parametrization.autodetect_parameters()
+@Parametrization.case(name="baseline", expected_results_file = str(ASSET_DIR/"fixtures/subtract1D/subtract1D_expected_results.pgl"))
+def test_subtract_1d(expected_results_file):
+
+    #get files
+    headerA,A=processFile(str(ASSET_DIR/"fixtures/file.A.sorted.annotated.pgl"))
+
+    headerB,B=processBedFile(str(ASSET_DIR/"fixtures/file.B.annotated.bed"))
+
+    args={}
+
+    results = subtract1D(A,B,args,headerA)
+    results = sorted(results)
+
+    new_header,expected_results = processFile(expected_results_file)
+
+    test_results = compare_test_outputs(results,expected_results)
+
+    assert test_results
